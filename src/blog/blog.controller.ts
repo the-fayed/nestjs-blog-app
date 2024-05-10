a3b5f6c08f97d580bdef12866d63060275f984b2
Ahemd Fayed <ll.ahmedfayed@gmail.com>
2024-05-10 23:37:37 +0300

    adding delete blog endpoint

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,17 +1,20 @@
 import { Pagination } from 'nestjs-typeorm-paginate';
 import {
-  UseGuards,
   Controller,
+  HttpStatus,
+  UseGuards,
+  HttpCode,
+  Delete,
   Param,
   Query,
+  Post,
   Body,
   Get,
-  Post,
   Put,
 } from '@nestjs/common';
 
 import { CurrentUser, Serialize } from '../decorators';
-import { IsAuthorGuard, JwtGuard } from '../guards';
+import { IsAuthorGuard, IsPermitted, JwtGuard } from '../guards';
 import { BlogService } from './blog.service';
 import { Blog } from './entity/blog.entity';
 import { IBlog } from './blog.interface';
@@ -19,9 +22,9 @@ import { User } from '../user';
 import {
   PaginatedBlogDto,
   GetAllBlogsDto,
+  UpdateBlogDto,
   CreateBlogDto,
   BlogDto,
-  UpdateBlogDto,
 } from './dtos';
 
 @Controller('api/v1/blogs')
@@ -29,8 +32,9 @@ export class BlogController {
   constructor(private readonly blogService: BlogService) {}
 
   @Post()
-  @UseGuards(JwtGuard)
   @Serialize(BlogDto)
+  @UseGuards(JwtGuard)
+  @HttpCode(HttpStatus.CREATED)
   public async create(
     @Body() createBlogDto: CreateBlogDto,
     @CurrentUser() user: User,
@@ -40,6 +44,7 @@ export class BlogController {
 
   @Get()
   @UseGuards(JwtGuard)
+  @HttpCode(HttpStatus.OK)
   @Serialize(PaginatedBlogDto)
   public async findAll(
     @Query() paginationOpt: GetAllBlogsDto,
@@ -47,13 +52,28 @@ export class BlogController {
     return this.blogService.findAll(paginationOpt);
   }
 
+  @Get(':id')
+  @Serialize(BlogDto)
+  @HttpCode(HttpStatus.OK)
+  public async findOne(@Param('id') id: number): Promise<IBlog> {
+    return this.blogService.findById(id);
+  }
+
   @Put(':id')
   @Serialize(BlogDto)
+  @HttpCode(HttpStatus.OK)
   @UseGuards(JwtGuard, IsAuthorGuard)
   public async updateOne(
     @Param('id') id: number,
     @Body() updateBlogDto: UpdateBlogDto,
   ): Promise<IBlog> {
     return this.blogService.updateOne(id, updateBlogDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @UseGuards(JwtGuard, IsPermitted)
+  public async deleteOne(@Param('id') id: number): Promise<void> {
+    return this.blogService.deleteOne(id);
   }
 }
--- src/guards/index.ts
+++ src/guards/index.ts
@@ -1,3 +1,4 @@
+export * from './is-permitted.guard';
 export * from './is-author.guard';
 export * from './auth.guard';
 export * from './role.guard';
